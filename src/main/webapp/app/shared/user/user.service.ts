380553137dbf31feb1b7f6fb5a10e14c2781011a
Matt Raible <matt.raible@okta.com>
2017-10-18 10:39:13 +0300

    Initial application generated by JHipster-4.10.0

---
+++ src/main/webapp/app/shared/user/user.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+
+import { SERVER_API_URL } from '../../app.constants';
+import { User } from './user.model';
+import { ResponseWrapper } from '../model/response-wrapper.model';
+import { createRequestOption } from '../model/request-util';
+
+@Injectable()
+export class UserService {
+    private resourceUrl = SERVER_API_URL + 'api/users';
+
+    constructor(private http: Http) { }
+
+    query(req?: any): Observable<ResponseWrapper> {
+        const options = createRequestOption(req);
+        return this.http.get(this.resourceUrl, options)
+            .map((res: Response) => this.convertResponse(res));
+    }
+
+    private convertResponse(res: Response): ResponseWrapper {
+        const jsonResponse = res.json();
+        return new ResponseWrapper(res.headers, jsonResponse, res.status);
+    }
+}
